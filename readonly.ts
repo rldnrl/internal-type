fc9ebfcd325bf47a57146046d92c4fb3ce96daec
enginecode-cpu <bassmatics1002@gmail.com>
2021-09-24 10:04:30 +0900

    readonly

---
+++ readonly.ts
@@ -0,0 +1,18 @@
+type MyReadonly<T> = { readonly [P in keyof T]: T[P] };
+
+type User = {
+  firstName: string;
+  lastName: string;
+  age: number;
+};
+
+const readonlyUser: MyReadonly<User> = {
+  firstName: "David",
+  lastName: "Thomas",
+  age: 25,
+};
+
+// Cannot assign to 'firstName' because it is a read-only property
+// readonlyUser.firstName = "Test First Name"
+// readonlyUser.lastName = "Test Last Name"
+// readonlyUser.age = 10
